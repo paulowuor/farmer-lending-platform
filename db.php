c3d9c300cfafb4a9287dabf74c0efb20900b3821
Paul Owuor <85417768+paulowuor@users.noreply.github.com>
2023-04-09 17:08:25 +0300

    Add files via upload

--- db.php
+++ db.php
@@ -1,9 +1,8 @@
-<?php
-    // Enter your host name, database username, password, and database name.
-    // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("localhost","root","","farmer");
-    // Check connection
+
+<?php 
+$con = mysqli_connect("localhost","root","","farmer");
     if (mysqli_connect_errno()){
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	die();
+	}
 ?>
